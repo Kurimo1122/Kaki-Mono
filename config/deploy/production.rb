a541c1ee8ffddd4dba2f73c7511558088c893f21
Toshimitsu Kugimoto <kugimoto1122@gmail.com>
2017-07-10 21:49:47 +0900

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server '13.113.247.189', user: 'ec2-user', roles: %w{app db web}
 
-#set :ssh_options, {
-#  keys: %w(/home/ec2-user/.ssh/id_rsa),
-#  forward_agent: true,
-#  auth_methods: %w(publickey)
-#}
+set :ssh_options, {
+  keys: %w(~/.ssh/key_pair.pem),
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
