e5b91ea22d34afc06f986bb66ae7124ad1f47a46
Toshimitsu Kugimoto <kugimoto1122@gmail.com>
2017-07-10 19:16:25 +0900

    Add landing page for AWS test

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module KakiMono
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # いらないファイルを作らない
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
